9dca00dc2254ec65cd12f4a15108a8e11554682c
Keller Perry <ikellerperry@gmail.com>
2023-09-28 15:41:52 -0700

    Complete core game functionality Add loop to play five rounds, not including Draws Add prompt for player's selection at start of each round Add counters for player and computer Add game winner announcement

--- script.js
+++ script.js
@@ -1,50 +1,59 @@
 
 // Randomly select and return the computer's choice:
-
 function getComputerChoice() {
     const choiceArr = ["rock", "paper", "scissors"];
     return choiceArr[Math.floor(Math.random() * 3)];
 }
+
+// Initializes round counters
+let playerCounter = 0;
+let computerCounter = 0;
   
-/* Prompt for and lowercase the user's selection. 
-        // let playerPrompt = prompt("Rock, Paper or Scissors?", "");
-        // playerSelection = playerPrompt.toLowerCase();
+//Game function call
+game();
 
-    // //Link the computerSelection parameter with the outcome of getComputerChoice
-        // computerSelection = getComputerChoice(); */
 
+//Loops through the playRound function five times, announces the winner and resets the counters
 function game() {
+    while((playerCounter + computerCounter) < 5) {
+        let playerPrompt = prompt("Rock, Paper or Scissors?", "");
+        playerSelection = playerPrompt.toLowerCase();
+        computerSelection = getComputerChoice();
+        
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if(computerCounter > playerCounter) {
+        console.log(`Oh no! You lost! ${playerCounter} to ${computerCounter}`);
+    } else {
+        console.log(`Congratulations! You win! ${playerCounter} to ${computerCounter}`);
+    }
 
-    
+    playerCounter = 0;
+    computerCounter = 0;
 }
 
-        
+
 // One round of Rock, Paper, Scissors
-
 function playRound(playerSelection, computerSelection) {
-
-    //Compare the competing selections and return a string declaring the winner
-
+    
+    //Capitalizes the first letter of each player's selection
     const prettyPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
     const prettyComputerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
 
+    //Compare the competing selections and return a string declaring the winner
     if(
         (computerSelection === "rock" && playerSelection === "scissors") ||(computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper")) {
+            computerCounter++
             return `You lose! ${prettyComputerSelection} beats ${prettyPlayerSelection}`;
     } else if(
         (computerSelection === "rock" && playerSelection === "rock") || (computerSelection === "paper" && playerSelection === "paper") || (computerSelection === "scissors" && playerSelection === "scissors")) {
-            return `It's a draw! You both chose ${prettyPlayerSelection}`
+            return `It's a draw! You both chose ${prettyPlayerSelection}.`
     } else {
+        playerCounter++
         return `You win! ${prettyPlayerSelection} beats ${prettyComputerSelection}`
     }
-
-
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
 
 
 
