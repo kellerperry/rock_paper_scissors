c04b572944a931bd6dd54c60436a3490c62140d1
Keller Perry <ikellerperry@gmail.com>
2023-09-27 22:27:37 -0700

    Add single round of Rock, Paper, Scissors play

--- script.js
+++ script.js
@@ -1,3 +1,73 @@
+
+// Randomly select and return the computer's choice:
+
+function getComputerChoice() {
+    const choiceArr = ["rock", "paper", "scissors"];
+    return choiceArr[Math.floor(Math.random() * 3)];
+}
+  
+/* Prompt for and lowercase the user's selection. 
+        // let playerPrompt = prompt("Rock, Paper or Scissors?", "");
+        // playerSelection = playerPrompt.toLowerCase();
+
+    // //Link the computerSelection parameter with the outcome of getComputerChoice
+        // computerSelection = getComputerChoice(); */
+
+function game() {
+
+    
+}
+
+        
+// One round of Rock, Paper, Scissors
+
+function playRound(playerSelection, computerSelection) {
+
+    //Compare the competing selections and return a string declaring the winner
+
+    const prettyPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    const prettyComputerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+
+    if(
+        (computerSelection === "rock" && playerSelection === "scissors") ||(computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper")) {
+            return `You lose! ${prettyComputerSelection} beats ${prettyPlayerSelection}`;
+    } else if(
+        (computerSelection === "rock" && playerSelection === "rock") || (computerSelection === "paper" && playerSelection === "paper") || (computerSelection === "scissors" && playerSelection === "scissors")) {
+            return `It's a draw! You both chose ${prettyPlayerSelection}`
+    } else {
+        return `You win! ${prettyPlayerSelection} beats ${prettyComputerSelection}`
+    }
+
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+/* Compare the user's selection to the computer's selection:
+        Rock is greater than Scissors
+        Scissors is greater than Paper
+        Paper is greater than Rock
+        If Paper equals Paper or Rock equals Rock or Scissors equals Scissors, the game is a draw.
+    Return a string that declares the player a winner, a loser or if the game is a draw.
+Loop through the one round of game to create a five-round game:
+    Initialize a player score counter and a computer score counter
+    After comparing the player's selection to the computer's selection increment the round winner's counter.
+    Log the round-end string to the console.
+    After a fifth round is completed, compare the player's score counter to the computer's score counter.
+    Log the game winner to the console */
+
+
+
+
+
+
+
 /*
 Randomly select and return Rock, Paper or Scissors for the computer's choice:
     Create an array of strings: "rock", "paper", "scissors";
@@ -19,13 +89,3 @@ Loop through the one round of game to create a five-round game:
     Log the round-end string to the console.
     After a fifth round is completed, compare the player's score counter to the computer's score counter.
     Log the game winner to the console */
-
-function getComputerChoice() {
-   const choiceArr = ["rock", "paper", "scissors"];
-   return choiceArr[Math.floor(Math.random() * 3)];
-}
-
-function playRound(playerSelection) {
-    playerSelection = prompt("Rock, Paper or Scissors?", "");
-    return playerSelection.toLowerCase();
-}
